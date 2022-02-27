2003a39d0c8f9e7a9c376adef5fad66d457f4399
HKristof2130 <78855300+HKristof2130@users.noreply.github.com>
2022-02-27 23:47:35 +0100

    Update main.c

--- 2.gyakorlat/rajzolas/main.c
+++ 2.gyakorlat/rajzolas/main.c
@@ -1,9 +1,5 @@
 ﻿#include <SDL.h>
 #include <SDL2_gfxPrimitives.h>
-
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -16,22 +12,72 @@ typedef struct Color{
 }Color;
 
 typedef struct Line{
-    float xCoord;
-    float yCoord;
+    int x1Coord;
+    int y1Coord;
+    int x2Coord ;
+    int y2Coord ;
     Color lineColor;
 }Line;
 
 void getColor(Color* c);
+void setLineColor(Line l);
 void setLine(Line* l);
 
 
 int main(int argc, char *argv[])
 {
     Line l1;
-    setLine(&l1);
-    printf("A vonal adatai:\nX koordinata: %f\nY koordinata: %f\nPiros: %d\nZold: %d\nKek: %d\n",l1.xCoord,l1.yCoord,l1.lineColor.red,l1.lineColor.green,l1.lineColor.blue);
+    l1.x2Coord = -1;
+    l1.y2Coord = -1;
+
+    // Initialize SDL
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Create a SDL window
+    SDL_Window *window = SDL_CreateWindow("Hello, SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 600, SDL_WINDOW_OPENGL);
+
+    // Create a renderer (accelerated and in sync with the display refresh rate)
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
 
+    bool running = true;
+    SDL_Event event;
+    while(running)
+    {
+        // Process events
+        while(SDL_PollEvent(&event))
+        {
+            if(SDL_MOUSEBUTTONDOWN){
+                SDL_GetMouseState(&l1.x1Coord,&l1.y1Coord);
+                while(l1.x2Coord < 0 && l1.y2Coord < 0){
+                    if(SDL_MOUSEBUTTONDOWN){
+                        SDL_GetMouseState(&l1.x2Coord,&l1.y2Coord);
+                    }
+                }
+                SDL_RenderDrawLine(renderer, l1.x1Coord, l1.y1Coord, l1.x2Coord, l1.y2Coord);
+            }
+
+            if(event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+        }
+
+        // Clear screen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Draw
+
+        // Show what was drawn
+        SDL_RenderPresent(renderer);
+    }
+
+    // Release resources
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    printf("x1: %d y1: %d \nx2: %d y2: %d \n",l1.x1Coord,l1.y1Coord,l1.x2Coord,l1.y2Coord);
     return 0;
 }
 
@@ -75,15 +121,20 @@ void getColor(Color* c){
 
 void setLine(Line* l){
 
-    float x,y;
+    int x,y;
     printf("Add meg az X koordinatat: ");
-    scanf("%f",&x);
+    scanf("%d",&x);
     printf("Add meg az Y koordinatat: ");
-    scanf("%f",&y);
-    l->xCoord = x;
-    l->yCoord = y;
+    scanf("%d",&y);
+    l->x1Coord = x;
+    l->y1Coord = y;
     getColor(&l->lineColor);
 
     return;
+
+}
+
+void setLineColor(Line l){
+
 
 }
